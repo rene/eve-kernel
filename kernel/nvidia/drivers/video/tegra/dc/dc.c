d22ee115b3a3c20f78afecb084df87ab61ce4b1b
Renê de Souza Pinto <rene@renesp.com.br>
2023-08-04 18:15:54 +0200

    nvidia: drivers: Fix kernel crash after warming reset
    Sometimes after a warming reset the kernel will crash during initialization
    with the following error:
    
    CPU:0, Error:cbb-noc@0x2300000,irq=15
    
    This commit fixes this problem. The patch was taken from:
    
    https://forums.developer.nvidia.com/t/cpu-0-error-cbb-noc-0x2300000-irq-15/230970/3
    
    Signed-off-by: Renê de Souza Pinto <rene@renesp.com.br>

--- kernel/nvidia/drivers/video/tegra/dc/dc.c
+++ kernel/nvidia/drivers/video/tegra/dc/dc.c
@@ -6378,7 +6378,7 @@ bool tegra_is_bl_display_initialized(int instance)
 		pr_debug("disable seamless set from DT\n");
 		return false;
 	}
-	return (dc->fb_mem->start != 0);
+	return false;
 }
 EXPORT_SYMBOL(tegra_is_bl_display_initialized);
 
